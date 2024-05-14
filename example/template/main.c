82c685c46c25f04c2abd95b090355ef301028ccf
wyxun <wyxun023768@outlook.com>
2024-05-14 19:31:08 +0800

    update template

--- example/template/main.c
+++ example/template/main.c
@@ -14,12 +14,35 @@ example_cfg_t tEexampleCfg = {
 };
 example_t tExample;
 
+void StorageWrite(uint16_t *phwStorageStartAddr, uint16_t hwStorageLength)
+{
+    // Write flash save sys_data programme
+}
+void StorageRead(uint16_t *phwStorageStartAddr, uint16_t hwStorageLength)
+{
+    // Write flash read sys_data programme
+}
+
+// Structures for sys_data access
+uint16_t g_hwSystemDataArrary[16];
+gstorage_data_t tSysData = {
+    .phwStorageStartAddr = g_hwSystemDataArrary,
+    .hwStorageLength = 16,
+    .hwCrcFlag = 0,
+    .fcnWrite = StorageWrite,
+    .fcnRead = StorageRead,
+};
+
+gmsi_t tGmsi = {&tSysData};
+
 int main()
 {   
     // example hardware init
 
     // example object init
     example_Init((uintptr_t)&tExample, (uintptr_t)&tEexampleCfg);
+    
+    gmsi_Init(&tGmsi);
     
     while (1)
     {
